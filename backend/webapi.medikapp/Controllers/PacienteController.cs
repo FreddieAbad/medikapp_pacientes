3631d58affbd5cdd4b5ec97a6118ea47b8d8e128
Freddie <freddy.abadl@ucuenca.edu.ec>
2020-09-19 15:00:55 -0500

    backend

---
+++ backend/webapi.medikapp/Controllers/PacienteController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.medikapp.Data;
+using webapi.medikapp.Entities;
+
+namespace webapi.medikapp.Controllers
+{
+    [Route("api/paciente")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+        private readonly PacienteDb _pacienteDb;
+
+        public PacienteController(PacienteDb pacienteDb)
+        {
+            _pacienteDb = pacienteDb;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_pacienteDb.Get());
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetCliente")]
+        public IActionResult GetById(string id)
+        {
+            var paciente = _pacienteDb.GetById(id);
+
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(paciente);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Paciente paciente)
+        {
+            _pacienteDb.Create(paciente);
+
+            return CreatedAtRoute("GetPaciente", new
+            {
+                id = paciente.id.ToString()
+            }, paciente);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Paciente cli)
+        {
+            var cliente = _pacienteDb.GetById(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            _pacienteDb.Update(id, cli);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult DeleteById(string id)
+        {
+            var paciente = _pacienteDb.GetById(id);
+
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            _pacienteDb.DeleteById(paciente.id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ backend/webapi.medikapp/Entities/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace webapi.medikapp.Entities
+{
+    //[BsonIgnoreExtraElements]
+    public class Paciente
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string id_doctor { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
+        public string direccion { get; set; }
+
+
+
+    }
+}
